f9a58020bf8b1f73aafcf1c65fb8a12b18c20137
unknown <vanspauwensebastiaan@gmail.com>
2022-10-18 21:03:33 +0200

    Connect loss recover

--- qube_backend/src/index.ts
+++ qube_backend/src/index.ts
@@ -5,71 +5,99 @@ import { destructure } from './helpers/destructure'
 import WebSocket, { WebSocketServer } from 'ws'
 import { QUBEPacket } from './types/packet.type'
 
-interface Info {
-  address: string
-  port: number
-}
-
 export class Runner {
-  server: Server
-  wss: WebSocketServer
+  wss: WebSocketServer = new WebSocketServer({ port: 9000 })
   private readonly logger: Logger = new Logger(LogLevel.INFO)
   private lastPacket: QUBEPacket | undefined
   private pinger: NodeJS.Timer
+  private pingReply: boolean = true
 
-  private readonly QUBE_PORT_SEND = 10016
-  private readonly QUBE_PORT_RECV = 7777
+  private readonly QUBE_PORT_RECV = 10016
+  private readonly QUBE_PORT_SEND = 7777
   private readonly QUBE_IP = '172.22.222.208'
 
+  private socket: datagram.Socket = datagram.createSocket('udp4') 
+
   async start (): Promise<void> {
-    try {
-      this.wss = new WebSocketServer({ port: 9000 })
-      this.logger.info('WebSocket server started on port 9000')
-    } catch (e) {
-      this.logger.error('Failed to start WebSocket server')
-      this.logger.error(e)
-    }
 
-    const socket = datagram.createSocket('udp4')
-    socket.bind(this.QUBE_PORT_SEND)
-
-    this.logger.info('QUBE_BACKEND server started')
-
-    socket.on('message', (raw: Buffer) => { 
-      this.lastPacket = destructure(raw)
-
-      if (this.lastPacket !== undefined) {
-        this.wss.clients.forEach(client => {
-          if (client.readyState === WebSocket.OPEN) {
-            this.logger.info('Sending packet to client')
-            client.send(JSON.stringify(this.lastPacket))
-          }
-        })
-      }
-    }) 
-
-    this.pinger = setInterval(() => {
-      socket.send('ping', this.QUBE_PORT_RECV, this.QUBE_IP)
-    }, 5000)
+    this.initHandlers()
+    await this.initPing()
     
     this.wss.on('connection', (ws: WebSocket) => {
-      console.log('Client connected from:', ws._socket.remoteAddress, 'on port:', ws._socket.remotePort)
+      this.logger.info('Client connected from:', ws)
     })
 
     this.wss.on('message', (message: string) => {
-      console.log('Message from client: ', message)
+      this.logger.info('Message from client: ', message)
     })
 
     process.on('SIGINT', () => { void this.close() })
     process.on('SIGTERM', () => { void this.close() })
   }
 
+  private initHandlers(): void {
+
+    this.logger.info('Connecting to QUBE')
+  
+    this.socket.bind(this.QUBE_PORT_RECV)
+    this.socket.connect(this.QUBE_PORT_SEND, this.QUBE_IP)
+
+    this.socket.on('error', (err: Error) => {
+      this.logger.error('Error: ', err)
+    })
+
+    this.socket.on('message', (raw: Buffer, rInfo: datagram.RemoteInfo) => { 
+
+      switch (rInfo.size) {
+
+        case 14:
+          this.logger.info('Reply QUBE at', this.socket.address().address + ':' + this.socket.address().port)
+          this.pingReply = true
+          break
+
+        case 512:
+          this.lastPacket = destructure(raw)
+
+          if (this.lastPacket !== undefined) {
+            this.wss.clients.forEach(client => {
+              if (client.readyState === WebSocket.OPEN) {
+                this.logger.info('Sending packet to client')
+                client.send(JSON.stringify(this.lastPacket))
+              }
+            })
+          }
+          break
+
+        default:
+          throw Error('Unknown packet size')
+      }
+    })
+  }
+
+  async initPing(): Promise<void> {
+    this.pinger = setInterval(() => {
+      this.logger.info('Pinged QUBE at', this.QUBE_IP + ':' + this.QUBE_PORT_SEND)
+
+      if (this.pingReply) {
+        this.socket.send('ping', this.QUBE_PORT_SEND, this.QUBE_IP)
+        this.pingReply = false
+      } else {
+        this.handlePingTimeout()
+      }
+    }, 2000)
+  }
+
+  async handlePingTimeout(): Promise<void> {
+    await this.close()
+    await new Runner().start()
+  }
+
   async close (): Promise<void> {
-    this.server?.close()
+    this.wss?.close()
+    this.socket.close()
     clearTimeout(this.pinger)
 
-    // eslint-disable-next-line no-console
-    console.log('server closed')
+    this.logger.info('Closing connection to QUBE')
   }
 }
 
